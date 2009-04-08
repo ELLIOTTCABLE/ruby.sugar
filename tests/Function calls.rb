73d18fc2c5c7ab8ad4cfe48da54ee1ba5c42d51c
elliottcable <git@elliottcable.name>
2009-04-07 19:05:34 -0800

    Added a slew of tests that test the parsing of method calls

---
+++ tests/Function calls.rb
@@ -0,0 +1,267 @@
+# global method, parenthesized arguments, newline terminated
+method()
+method()+123
+method()+orly?(42)
+method(123)
+method(orly?(42))
+method(arg1)+123
+method(arg1+123)
+method(arg1+123, arg2, what(456))
+method(arg1+123, arg2, what(456), *args)
+method(arg1+123, arg2, what(456), *args, &block)
+method(arg1+123, arg2,
+       what(456),
+       *args, &block)
+# global method, whitespace-delimited arguments, newline terminated
+method
+method+123
+method+orly? 42
+method 123
+method orly? 42
+method arg1 + 123
+method arg1 + 123, arg2, what 456
+method arg1 + 123, arg2, what 456, *args
+method arg1 + 123, arg2, what 456, *args, &block
+method arg1 + 123, arg2,
+       what 456,
+       *args, &block
+# instance method, parenthesized arguments, newline terminated
+foo.method()
+foo.method()+123
+foo.method()+orly?(42)
+foo.method(123)
+foo.method(orly?(42))
+foo.method(arg1)+123
+foo.method(arg1+123)
+foo.method(arg1+123, arg2, what(456))
+foo.method(arg1+123, arg2, what(456), *args)
+foo.method(arg1+123, arg2, what(456), *args, &block)
+foo.method(arg1+123, arg2,
+           what(456),
+           *args, &block)
+# instance method, whitespace-delimited arguments, newline terminated
+foo.method
+foo.method+123
+foo.method+orly? 42
+foo.method 123
+foo.method orly? 42
+foo.method arg1 + 123
+foo.method arg1 + 123, arg2, what 456
+foo.method arg1 + 123, arg2, what 456, *args
+foo.method arg1 + 123, arg2, what 456, *args, &block
+foo.method arg1 + 123, arg2,
+           what 456,
+           *args, &block
+# class method, parenthesized arguments, newline terminated
+Something.method()
+Something.method()+123
+Something.method()+orly?(42)
+Something.method(123)
+Something.method(orly?(42))
+Something.method(arg1)+123
+Something.method(arg1+123)
+Something.method(arg1+123, arg2, what(456))
+Something.method(arg1+123, arg2, what(456), *args)
+Something.method(arg1+123, arg2, what(456), *args, &block)
+Something.method(arg1+123, arg2,
+                 what(456),
+                 *args, &block)
+# class method, whitespace-delimited arguments, newline terminated
+Something.method
+Something.method+123
+Something.method+orly? 42
+Something.method 123
+Something.method orly? 42
+Something.method arg1 + 123
+Something.method arg1 + 123, arg2, what 456
+Something.method arg1 + 123, arg2, what 456, *args
+Something.method arg1 + 123, arg2, what 456, *args, &block
+Something.method arg1 + 123, arg2,
+                 what 456,
+                 *args, &block
+# global method, parenthesized arguments, expression-terminator terminated
+method(); method()
+method()+123; method()+123
+method()+orly?(42); method()+orly?(42)
+method(123); method(123)
+method(orly?(42)); method(orly?(42))
+method(arg1)+123; method(arg1)+123
+method(arg1+123); method(arg1+123)
+method(arg1+123, arg2, what(456)); method(arg1+123, arg2, what(456))
+method(arg1+123, arg2, what(456), *args); method(arg1+123, arg2, what(456), *args)
+method(arg1+123, arg2, what(456), *args, &block); method(arg1+123, arg2, what(456), *args, &block)
+method(arg1+123, arg2,
+       what(456),
+       *args, &block); method(arg1+123, arg2,
+                              what(456),
+                              *args, &block)
+# global method, whitespace-delimited arguments, expression-terminator terminated
+method; method
+method+123; method+123
+method+orly? 42; method+orly? 42
+method 123; method 123
+method orly? 42; method orly? 42
+method arg1 + 123; method arg1 + 123
+method arg1 + 123, arg2, what 456; method arg1 + 123, arg2, what 456
+method arg1 + 123, arg2, what 456, *args; method arg1 + 123, arg2, what 456, *args
+method arg1 + 123, arg2, what 456, *args, &block; method arg1 + 123, arg2, what 456, *args, &block
+method arg1 + 123, arg2,
+       what 456,
+       *args, &block; method arg1 + 123, arg2,
+                             what 456,
+                             *args, &block
+# instance method, parenthesized arguments, expression-terminator terminated
+foo.method(); foo.method()
+foo.method()+123; foo.method()+123
+foo.method()+orly?(42); foo.method()+orly?(42)
+foo.method(123); foo.method(123)
+foo.method(orly?(42)); foo.method(orly?(42))
+foo.method(arg1)+123; foo.method(arg1)+123
+foo.method(arg1+123); foo.method(arg1+123)
+foo.method(arg1+123, arg2, what(456)); foo.method(arg1+123, arg2, what(456))
+foo.method(arg1+123, arg2, what(456), *args); foo.method(arg1+123, arg2, what(456), *args)
+foo.method(arg1+123, arg2, what(456), *args, &block);foo.method(arg1+123, arg2, what(456), *args, &block)
+foo.method(arg1+123, arg2,
+           what(456),
+           *args, &block); foo.method(arg1+123, arg2,
+                                      what(456),
+                                      *args, &block)
+# instance method, whitespace-delimited arguments, expression-terminator terminated
+foo.method; foo.method
+foo.method+123; foo.method+123
+foo.method+orly? 42; foo.method+orly? 42
+foo.method 123; foo.method 123
+foo.method orly? 42; foo.method orly? 42
+foo.method arg1 + 123; foo.method arg1 + 123
+foo.method arg1 + 123, arg2, what 456; foo.method arg1 + 123, arg2, what 456
+foo.method arg1 + 123, arg2, what 456, *args; foo.method arg1 + 123, arg2, what 456, *args
+foo.method arg1 + 123, arg2, what 456, *args, &block; foo.method arg1 + 123, arg2, what 456, *args, &block
+foo.method arg1 + 123, arg2,
+           what 456,
+           *args, &block; foo.method arg1 + 123, arg2,
+                                     what 456,
+                                     *args, &block
+# class method, parenthesized arguments, expression-terminator terminated
+Something.method(); Something.method()
+Something.method()+123; Something.method()+123
+Something.method()+orly?(42); Something.method()+orly?(42)
+Something.method(123); Something.method(123)
+Something.method(orly?(42)); Something.method(orly?(42))
+Something.method(arg1)+123; Something.method(arg1)+123
+Something.method(arg1+123); Something.method(arg1+123)
+Something.method(arg1+123, arg2, what(456)); Something.method(arg1+123, arg2, what(456))
+Something.method(arg1+123, arg2, what(456), *args); Something.method(arg1+123, arg2, what(456), *args)
+Something.method(arg1+123, arg2, what(456), *args, &block); Something.method(arg1+123, arg2, what(456), *args, &block)
+Something.method(arg1+123, arg2,
+                 what(456),
+                 *args, &block); Something.method(arg1+123, arg2,
+                                                  what(456),
+                                                  *args, &block)
+# class method, whitespace-delimited arguments, expression-terminator terminated
+Something.method; Something.method
+Something.method+123; Something.method+123
+Something.method+orly? 42; Something.method+orly? 42
+Something.method 123; Something.method 123
+Something.method orly? 42; Something.method orly? 42
+Something.method arg1 + 123; Something.method arg1 + 123
+Something.method arg1 + 123, arg2, what 456; Something.method arg1 + 123, arg2, what 456
+Something.method arg1 + 123, arg2, what 456, *args; Something.method arg1 + 123, arg2, what 456, *args
+Something.method arg1 + 123, arg2, what 456, *args, &block; Something.method arg1 + 123, arg2, what 456, *args, &block
+Something.method arg1 + 123, arg2,
+                 what 456,
+                 *args, &block; Something.method arg1 + 123, arg2,
+                                                 what 456,
+                                                 *args, &block
+# global method, parenthesized arguments, expression-end terminated
+(method()).method()
+(method()+123).method()+123
+(method()+orly?(42)).method()+orly?(42)
+(method(123)).method(123)
+(method(orly?(42))).method(orly?(42))
+(method(arg1)+123).method(arg1)+123
+(method(arg1+123)).method(arg1+123)
+(method(arg1+123, arg2, what(456))).method(arg1+123, arg2, what(456))
+(method(arg1+123, arg2, what(456), *args)).method(arg1+123, arg2, what(456), *args)
+(method(arg1+123, arg2, what(456), *args, &block)).method(arg1+123, arg2, what(456), *args, &block)
+(method(arg1+123, arg2,
+       what(456),
+       *args, &block)).method(arg1+123, arg2,
+                              what(456),
+                              *args, &block)
+# global method, whitespace-delimited arguments, expression-end terminated
+(method).method
+(method+123).method+123
+(method+orly? 42).method+orly? 42
+(method 123).method 123
+(method orly? 42).method orly? 42
+(method arg1 + 123).method arg1 + 123
+(method arg1 + 123, arg2, what 456).method arg1 + 123, arg2, what 456
+(method arg1 + 123, arg2, what 456, *args).method arg1 + 123, arg2, what 456, *args
+(method arg1 + 123, arg2, what 456, *args, &block).method arg1 + 123, arg2, what 456, *args, &block
+(method arg1 + 123, arg2,
+       what 456,
+       *args, &block).method arg1 + 123, arg2,
+                             what 456,
+                             *args, &block
+# instance method, parenthesized arguments, expression-end terminated
+(foo.method()).foo.method()
+(foo.method()+123).foo.method()+123
+(foo.method()+orly?(42)).foo.method()+orly?(42)
+(foo.method(123)).foo.method(123)
+(foo.method(orly?(42))).foo.method(orly?(42))
+(foo.method(arg1)+123).foo.method(arg1)+123
+(foo.method(arg1+123)).foo.method(arg1+123)
+(foo.method(arg1+123, arg2, what(456))).foo.method(arg1+123, arg2, what(456))
+(foo.method(arg1+123, arg2, what(456), *args)).foo.method(arg1+123, arg2, what(456), *args)
+(foo.method(arg1+123, arg2, what(456), *args, &block)).foo.method(arg1+123, arg2, what(456), *args, &block)
+(foo.method(arg1+123, arg2,
+           what(456),
+           *args, &block)).foo.method(arg1+123, arg2,
+                                      what(456),
+                                      *args, &block)
+# instance method, whitespace-delimited arguments, expression-end terminated
+(foo.method).foo.method
+(foo.method+123).foo.method+123
+(foo.method+orly? 42).foo.method+orly? 42
+(foo.method 123).foo.method 123
+(foo.method orly? 42).foo.method orly? 42
+(foo.method arg1 + 123).foo.method arg1 + 123
+(foo.method arg1 + 123, arg2, what 456).foo.method arg1 + 123, arg2, what 456
+(foo.method arg1 + 123, arg2, what 456, *args).foo.method arg1 + 123, arg2, what 456, *args
+(foo.method arg1 + 123, arg2, what 456, *args, &block).foo.method arg1 + 123, arg2, what 456, *args, &block
+(foo.method arg1 + 123, arg2,
+           what 456,
+           *args, &block).foo.method arg1 + 123, arg2,
+                                     what 456,
+                                     *args, &block
+# class method, parenthesized arguments, expression-end terminated
+(Something.method()).Something.method()
+(Something.method()+123).Something.method()+123
+(Something.method()+orly?(42)).Something.method()+orly?(42)
+(Something.method(123)).Something.method(123)
+(Something.method(orly?(42))).Something.method(orly?(42))
+(Something.method(arg1)+123).Something.method(arg1)+123
+(Something.method(arg1+123)).Something.method(arg1+123)
+(Something.method(arg1+123, arg2, what(456))).Something.method(arg1+123, arg2, what(456))
+(Something.method(arg1+123, arg2, what(456), *args)).Something.method(arg1+123, arg2, what(456), *args)
+(Something.method(arg1+123, arg2, what(456), *args, &block)).Something.method(arg1+123, arg2, what(456), *args, &block)
+(Something.method(arg1+123, arg2,
+                 what(456),
+                 *args, &block)).Something.method(arg1+123, arg2,
+                                                  what(456),
+                                                  *args, &block)
+# class method, whitespace-delimited arguments, expression-end terminated
+(Something.method).Something.method
+(Something.method+123).Something.method+123
+(Something.method+orly? 42).Something.method+orly? 42
+(Something.method 123).Something.method 123
+(Something.method orly? 42).Something.method orly? 42
+(Something.method arg1 + 123).Something.method arg1 + 123
+(Something.method arg1 + 123, arg2, what 456).Something.method arg1 + 123, arg2, what 456
+(Something.method arg1 + 123, arg2, what 456, *args).Something.method arg1 + 123, arg2, what 456, *args
+(Something.method arg1 + 123, arg2, what 456, *args, &block).Something.method arg1 + 123, arg2, what 456, *args, &block
+(Something.method arg1 + 123, arg2,
+                 what 456,
+                 *args, &block).Something.method arg1 + 123, arg2,
+                                                 what 456,
+                                                 *args, &block
